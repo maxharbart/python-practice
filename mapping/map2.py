16ff530518a4c2070285c0aeddccb79a4bec2271
Max Garbart <maxharbart@yandex.ru>
2021-10-05 04:58:54 +0200

    icon color depends on height, height in popup

--- mapping/map2.py
+++ mapping/map2.py
@@ -5,17 +5,27 @@ data = pandas.read_csv('Volcanoes.csv')
 latitude = list(data['LAT'])
 longitude = list(data['LON'])
 name = list(data['NAME'])
+elev = list(data['ELEV'])
 
 
 m = folium.Map(location=[45.5236, -122.6750])
 
+def elev_color(x):
+    if x > 3000:
+        return 'red'   
+    elif x < 3000 and x > 2000:
+        return 'orange'
+    elif x < 2000 and x > 2000:
+        return 'yellow'
+    else:
+        return 'green'
 
 
 for i in range(0, len(data.index)):
     folium.Marker(
         location=[latitude[i], longitude[i]],
-        popup=name[i],
-        icon=folium.Icon(icon='fire')
+        popup=name[i] + ' ' + str(elev[i]) + ' meters',
+        icon=folium.Icon(icon='fire', color=elev_color(elev[i]))
         ).add_to(m)
 
 
