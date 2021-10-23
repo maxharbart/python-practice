28fb959827e0e705aecb7f94120034c60f29c17d
Max Garbart <maxharbart@yandex.ru>
2021-10-23 05:29:52 +0200

    *

--- processing.py
+++ processing.py
@@ -3,6 +3,8 @@ import cv2
 img = cv2.imread('interior.png')
 
 
-cv2.imshow('interior', img)
+img2 = cv2.resize(img,(int(img.shape[1]/3),int(img.shape[0]/3)))
+
+cv2.imshow('interior', img2)
 cv2.waitKey(0) 
 cv2.destroyAllWindows()
